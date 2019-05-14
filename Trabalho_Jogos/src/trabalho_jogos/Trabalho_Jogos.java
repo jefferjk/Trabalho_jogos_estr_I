d97429b45e598c63fc5d0aa684e6aa88fe3c01f2
MX1994 <41974205+MX1994@users.noreply.github.com>
2019-05-14 20:52:28 -0300

    Commit_2
    Trabalho finalizado

--- Trabalho_Jogos/src/trabalho_jogos/Trabalho_Jogos.java
+++ Trabalho_Jogos/src/trabalho_jogos/Trabalho_Jogos.java
@@ -21,8 +21,10 @@ public class Trabalho_Jogos {
     public static void main(String[] args) {
         
        TreeMap<String, Apoio> map = new TreeMap<String, Apoio>();
-           int total = 0;
-     try {
+        
+        int total = 0;
+        
+        try {
             BufferedReader br = new BufferedReader(new FileReader("game-reviews.csv"));
             String linha = null;
             String[] linhaSplit = null;
@@ -44,22 +46,63 @@ public class Trabalho_Jogos {
                     g.setAmazingPercentage(g.getAmazingPercentage() + 1);
                 }
 
-               
+                if (linhaSplit[2].equals("Masterpiece") || linhaSplit[2].equals("masterpiece")) {
+                    g.setMasterpiece(g.getMasterpiece() + 1);
+                }
+
+                if (linhaSplit[4].equals("Action")) {
+                    g.setPlataforma(g.getPlataforma() + 1);
+                    g.getScoresNintendo().add(Double.valueOf(linhaSplit[3]));
+                }
+
+                if (g.getMelhorJogo() == null || g.getMelhorJogoAvaliado() < Double.valueOf(linhaSplit[3])) {
+                    g.setMelhorJogo(linhaSplit[0]);
+                    g.setMelhorJogoAvaliado(Double.valueOf(linhaSplit[3]));
+                }
+
+                if (g.getPiorJogo() == null || g.getPiorJogoAvaliado() > Double.valueOf(linhaSplit[3])) {
+                    g.setPiorJogo(linhaSplit[0]);
+                    g.setPiorJogoAvaliado(Double.valueOf(linhaSplit[3]));
+                }
             }
             br.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
-     // INICIA A IMPRESSÃO DOS DADOS
-      
+        String plataforma = null;
+        
+     
+
+        // INICIA A IMPRESSÃO DOS DADOS
+        double console = 0;
+        double median = 0.0;
         for (String k : map.keySet()) {
             System.out.println("------------------------------------------------------------------");
             System.out.println("Gênero: " + k );
             System.out.println("");
+            System.out.println("Número de reviews: " + map.get(k).getNReviews());
             System.out.println("Percentual de Reviews do tipo 'Amazing': " + (map.get(k).getAmazingPercentage()));
+            System.out.println("Média Aritmética: " +  map.get(k).getMedia());
+            System.out.println("Desvio Padrão: " + map.get(k).getDesvioPadrao());
+            System.out.println("Melhor jogo: " + map.get(k).getMelhorJogo());
+            System.out.println("Pior jogo: " + map.get(k).getPiorJogo());
+
+
+            if (plataforma == "null" || console < map.get(k).getPlataforma()) {
+                if (map.get(k).getMediaImpressao()> console) {
+                    plataforma = k;
+                    console = map.get(k).getMediaImpressao();
+                    
+                }
+            }
 
         }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("Genero mais comum da Nintendo: " + plataforma);
+        System.out.println("");
+        System.out.println("");
     }
-    
 }
